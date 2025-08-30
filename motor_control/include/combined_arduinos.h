77a70fa49d59befc1b507ab2836c48d397b2db01
HaYoung <128967811+KkomMs@users.noreply.github.com>
2025-08-31 00:30:25 +0900

    Create combined_arduinos.h

---
+++ motor_control/include/combined_arduinos.h
@@ -0,0 +1,9 @@
+// control_interface.h
+#pragma once
+
+void initArduinos();
+void sendToLinear(int motor, int direction, int distance_mm);
+void sendToicemaker(int function_num, int repeat);
+void sendTocoffeemachine(int function_num);
+void sendTosyrup(int function_num);
+void closeSerials();
