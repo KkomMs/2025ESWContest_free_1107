c63aef56ec3b4d908399696eaec771bcb05ee0f6
HaYoung <128967811+KkomMs@users.noreply.github.com>
2025-09-04 17:56:19 +0900

    add LED function

--- motor_control/include/combined_arduinos.h
+++ motor_control/include/combined_arduinos.h
@@ -6,4 +6,5 @@ void sendToLinear(int motor, int direction, int distance_mm);
 void sendToicemaker(int function_num, int repeat);
 void sendTocoffeemachine(int function_num);
 void sendTosyrup(int function_num);
+void sendToLED(int function_num);
 void closeSerials();
