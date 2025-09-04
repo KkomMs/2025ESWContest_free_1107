0256ea7ce0399cec6de7ac174a7ea0d12fcd8195
HaYoung <128967811+KkomMs@users.noreply.github.com>
2025-09-04 17:57:06 +0900

    edit station

--- motor_control/include/combined_dxl.h
+++ motor_control/include/combined_dxl.h
@@ -5,6 +5,10 @@
 
 void setupDXL();
 void moveGripper(int32_t goal_velocity, float revolutions, int direction);   
-void moveDispenser(int32_t goal_velocity, float revolutions, int direction);
-void Station(int station_num, int32_t goal_velocity, float revolutions, int direction);
-void closeDXL();
+void moveDispenser(int32_t goal_velocity, float revolutions, int direction); 
+void moveFirstStation(int32_t goal_velocity, float revolutions, int direction);
+void moveSecondStation(int32_t goal_velocity, float revolutions, int direction);
+void moveThirdStation(int32_t goal_velocity, float revolutions, int direction);
+void moveFourthStation(int32_t goal_velocity, float revolutions, int direction);
+void closeGripper();
+void closeDispenser();
